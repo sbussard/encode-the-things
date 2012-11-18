e816fb38a0d6bd335c9ae3aac29128c4246349a4
Stephen Bussard <me@stephenbussard.com>
2012-11-18 13:28:00 -0700

    decode function and different default base

--- base_encode.js
+++ base_encode.js
@@ -1,12 +1,20 @@
-function base_encode(n, map) {
+/**
+* base_encode
+* @returns
+* The string value of decimal n mapped to base map
+* @params
+* n - a decimal number
+* map - a string representation of the base to be mapped to
+*/
+var base_encode = function (n, map) {
   var rem,
     arr = [],
-    num = n || 0,
-    charmap = map || "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    num = n || '',
+    charmap = map || "123456789ABCDEFGHIJKLMNPQRSTUVWXYZ",
     base = charmap.length;
 
-  if (num === 0) {
-    return charmap[0];
+  if (num === '') {
+    return '';
   }
 
   while (num) {
@@ -16,6 +24,33 @@ function base_encode(n, map) {
   }
 
   return arr.reverse().join('');
-}
+};
 
-console.log(base_encode((new Date().getTime()), "123456789ABCDEFGHIJKLMNPQRSTUVWXYZ"));
+
+/**
+* base_decode
+* @returns
+* The decoded decimal value of m
+* @params
+* m - the value of an encoded decimal number that has been mapped to map
+* map - a string of characters that represents the base that m has been mapped to
+*/
+var base_decode = function(m, map) {
+  var charmap = map || "123456789ABCDEFGHIJKLMNPQRSTUVWXYZ";
+  res = 0;
+  for(var i = 0; i < m.length; i++) {
+    var x = Math.pow(charmap.length, (m.length - i - 1));
+    var val = parseInt(charmap.indexOf(m.charAt(i)), 10);
+    res += x * val;
+  }
+  
+  return res;
+};
+
+// if it's a node.js module, export the functions
+if(typeof module !== "undefined") {
+  module.exports = {
+    base_encode: base_encode,
+    base_decode: base_decode
+  };
+}
